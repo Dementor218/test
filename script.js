8f6554efe7a9f1177f0e53ea21773031aec66ca6
Jonh <gk1993@rambler.ru>
2018-07-16 19:39:49 +0100

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,59 @@
+var first = 1;
+
+console.log(first);
+
+/*var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolen = true;
+null;
+undefined;
+var obj = ();*/
+
+console.log(4/0);
+console.log('string' * 9);
+console.log('Привет, ' + 'Мир!');
+//console.log(too)
+
+var ti;
+console.log(ti);
+
+obj = {
+	name: "Jonh",
+	age: 23,
+	isMarried: false
+}
+
+console.log(obj["age"]);
+
+var arr = ['plum', 'orange', 'apple'];
+console.log(arr[1]);
+
+//alert('hello!');
+//confirm("Are you here?");
+//var answer = +prompt("Вам есть 18?", "18");
+
+//console.log( typeof(answer));
+
+/*console.log('arr' + ' - object');
+console.log( 4 + ' - object');*/
+
+var incr = 10;
+		decr = 10;
+
+//incr++;
+//decr--;
+
+//console.log(incr++);
+//console.log(decr--);
+
+console.log(incr == 10);
+
+var isChecked = true;
+var isClose = true;
+
+console.log(isChecked || !isClose);
+
+var lest = 10;
+var thr = 11;
+console.log( lest + thr );
